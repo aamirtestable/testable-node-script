68543251216261ffede7083ec728e2a81361a951
Aamir Mahmood <67591625+aamirtestable@users.noreply.github.com>
2021-08-30 19:48:12 +0500

    Update and rename test2.js to test.ts

---
+++ test.ts
@@ -0,0 +1,2 @@
+import * as http from 'http';
+http.get('http://sample.testable.io/stocks/IBM');
--- test2.js
+++
@@ -1,8 +0,0 @@
-const rp = require('request-promise');
-const assert = require('assert');
-describe('My Test Suite 2', function() {
-  it('An example test step 2', async function() {
-    const quote = await rp({ uri: 'http://sample.testable.io/stocks/IBM', json: true });
-    assert(quote.symbol === 'IBM', 'Symbol of the quote should be IBM');
-  });
-});
